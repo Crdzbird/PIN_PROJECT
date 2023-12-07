bc2dd7d765138335be8035bf511b390517b6840f
edoelas <miego.cvcv@gmail.com>
2023-12-07 11:17:20 +0100

    add out

---
+++ scripts/ff.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# FILEPATH: /home/alumno.upv.es/miedgo/W/PIN_PROJECT/scripts/ff.sh
+
+./planners/ff/metric_ff -o ./me_ascensores/domain.pddl -f ./me_ascensores/problem.pddl > ./out/ff.out
